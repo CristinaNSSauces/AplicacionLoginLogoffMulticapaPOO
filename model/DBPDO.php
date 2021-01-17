9422a626b4b7c3fb8555d1cd7e76f8a6232e8dc4
CristinaNS <cristina.nunseb@educa.jcyl.es>
2021-01-17 17:34:21 +0100

    Add registration functionality and documentation

--- model/DBPDO.php
+++ model/DBPDO.php
@@ -1,7 +1,26 @@
 <?php
+/**
+ * Class DBPDO
+ *
+ * Clase cuyo metodo permite establecer una conexion con la base de datos 
+ * 
+ * @author Cristina Nuñez y Javier Nieto
+ * @since 1.0
+ * @copyright 16-01-2021
+ * @version 1.0
+ */
 
 class DBPDO {
 
+/**
+ * Metodo ejecutarConsulta()
+ * 
+ * Metodo que nos permite ejecutar una consulta sql a la base de datos
+ * 
+ * @param string $sentenciaSQL sentencia sql que queremos ejecutar
+ * @param array $parametros parametros que necesita la consulta
+ * @return null|PDOStatement resultado que devolverá la consulta
+ */
     public static function ejecutaConsulta($sentenciaSQL, $parametros) {
         try {
             $miDB = new PDO(DNS, USER, PASSWORD);
@@ -10,9 +29,9 @@ class DBPDO {
             $consulta->execute($parametros); //Ejecutamos la consulta.
         } catch (PDOException $exception) {
             $consulta = null; //Destruimos la consulta.
-            echo $exception->getMessage();//Mostramos el mensaje de error de la excepcion
-            unset($miDB);//Cerramos la conexion de la base de datos
+            echo $exception->getMessage();
+            unset($miDB);
         }
-        return $consulta;//Devolvemos la consulta
+        return $consulta;
     }
 }
