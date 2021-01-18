ea519a85015712159a49f025485a254d5d13db8b
Cristina Nuñez <cristina.nunseb@educa.jcyl.es>
2021-01-18 13:37:26 +0100

    v1.1 LoginLogoffMulticapaPOO

--- view/layout.php
+++ view/layout.php
@@ -14,9 +14,10 @@
             <?php require_once $vistaEnCurso ?>
 
         <footer class="fixed">
-            <a href="http://daw217.ieslossauces.es/" target="_blank"> <img src="webroot/media/oneandone.png" alt="oneandone icon" width="40"></a>
-            <address>  <a href="../../index.html">&copy; 2020-2021 Javier Nieto Lorenzo</a> </address>
-            <a href="https://github.com/JavierNLSauces/" target="_blank"><img class="github" width="40" src="webroot/media/github.png" ></a>
+                <a href="http://daw215.ieslossauces.es/" target="_blank"> <img src="webroot/media/oneandone.png" alt="oneandone icon" width="40"></a>
+                <address>  <a href="../../index.html">&copy; 2020-2021 Cristina Núñez Sebastián</a> </address>
+                <a href="doc/index.html" target="_blank"><img class="github" width="40" src="webroot/media/doc.png" ></a>
+                <a href="https://github.com/CristinaNSSauces/" target="_blank"><img class="github" width="40" src="webroot/media/github.png" ></a>
         </footer>
     </body>
 </html>
